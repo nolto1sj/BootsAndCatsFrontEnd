87179d82a238d9c3702e4eb62d6ccd7a6debadd0
nolto1sj <sjnolton@gmail.com>
2022-12-01 20:17:10 -0500

    removed oauth import

--- src/app/services/boots-and-cats-backend.service.ts
+++ src/app/services/boots-and-cats-backend.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '@auth0/auth0-angular';
+import { User } from '../interfaces/user'
 import { Observable } from 'rxjs';
 import { Review } from '../interfaces/review';
 
