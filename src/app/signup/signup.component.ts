e7fc2fac1bed8d10f491fddbd2dece16232b6254
MacobJagyar <jacobmagyar95@gmail.com>
2022-12-02 17:30:12 -0500

    Tinkered with ngForm using unit 9 (restaurant faves) as example. Couldn't get form to create user to DB

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UrlSerializer } from '@angular/router';
+import { User } from '../interfaces/user';
+import { BootsAndCatsBackendService } from '../services/boots-and-cats-backend.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+@Output() userSave = new EventEmitter<User>();
 
-  constructor() { }
+
+  FirstName: string ='';
+  LastName: string = '';
+  UserName: string = '';
+  Password: string = '';
+
+  constructor(private service: BootsAndCatsBackendService) { }
+
 
   ngOnInit(): void {
   }
+
+  submit = (): void => {
+    this.userSave.emit({
+      FirstName: this.FirstName,
+      LastName: this.LastName,
+      UserName: this.UserName,
+      Password: this.Password
+    });
+    this.FirstName = '';
+    this.LastName = '';
+    this.UserName = '';
+    this.Password = '';
+  };
+
+  addUser = (user: User): void => {
+    this.service.signup(user).subscribe();
+  }
+
+// addUser(formParam: NgForm): void {
+//   let newUser: User = {
+//     FirstName: formParam.form.value.fname,
+//     LastName: formParam.form.value.lname,
+//     UserName: formParam.form.value.uname,
+//     Password: formParam.form.value.pass
+//   }
+
+//   this.service.signup(newUser);
+// }
 
 }
