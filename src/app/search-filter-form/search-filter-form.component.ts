5f981ff0a88ff6dbd7eaa0e400716a849864c168
nolto1sj <sjnolton@gmail.com>
2022-12-03 13:08:11 -0500

    Correctly accessed the albums property on Search feature

--- src/app/search-filter-form/search-filter-form.component.ts
+++ src/app/search-filter-form/search-filter-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SpotifyService } from '../services/spotify.service'
-import { Album, SearchFeature } from '../interfaces/search-feature';
+import { Albums, Album } from '../interfaces/album';
+import { SearchFeature } from '../interfaces/search-feature';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,8 +12,8 @@ import { Observable } from 'rxjs';
 export class SearchFilterFormComponent implements OnInit {
   searchQuery: string = '';
   searchAPIResponse: SearchFeature = {}  as SearchFeature;
-  albumResponse: Album[] = this.searchAPIResponse.albums //this is not correctly populating the array because the interface is set up incorrectly
-  
+  albumResponse: Albums = {} as Albums
+  albumArray: Album[] = []
 
 constructor(private spotifyService: SpotifyService) { }
 
@@ -21,6 +22,8 @@ constructor(private spotifyService: SpotifyService) { }
 
   searchAlbums(){
   this.spotifyService.getAllAlbums(this.searchQuery).subscribe((data: SearchFeature) => {this.searchAPIResponse = data;})
+  this.albumResponse = this.searchAPIResponse.albums
+  this.albumArray = this.albumResponse.albums
   }
 }
  
