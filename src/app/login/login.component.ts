840f8bd11f78905b42a2ca5aa34a59d763c53bab
morgan <pttrsn.mrgn@gmail.com>
2022-12-04 11:25:08 -0600

    added get all users endpoint and funcitonality to select user upon login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,33 +1,39 @@
 import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from '@auth0/auth0-angular';
 import { BootsAndCatsBackendService } from '../services/boots-and-cats-backend.service';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private service: BootsAndCatsBackendService
+  ) {}
 
-  constructor(private router: Router, private service: BootsAndCatsBackendService) { }
+  allUsers = this.service.getAllUser;
 
-  selectedUser: User= this.selectUser;
-  allUsers = this.service.getAllUser();
+  selectedUser: User = {} as User;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectUser(userForm: User): void {
+    for (let i = 0; i < this.allUsers.length; i++) {
+      if (
+        userForm.UserName &&
+        userForm.Password == (this.allUsers as any)[i].UserName &&
+        (this.allUsers as any)[i].Password
+      ) {
+        userForm = this.selectedUser;
+      }
+    }
   }
-
-selectUser(user: User): {
-
-
- 
-}
-
 
   signUpRedirect(): void {
     this.router.navigate(['/signup']);
-
   }
 }
