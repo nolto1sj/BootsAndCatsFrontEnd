aeec6d64214eb7f0fa7280c3dae784f759663764
Tolu <tsorinmade@yahoo.com>
2022-12-06 21:37:38 -0800

    Commented out methods to redirect and bring in album id

--- src/app/add-review-form/add-review-form.component.ts
+++ src/app/add-review-form/add-review-form.component.ts
@@ -9,7 +9,7 @@ import { LoginComponent } from '../login/login.component';
   styleUrls: ['./add-review-form.component.css']
 })
 export class AddReviewFormComponent implements OnInit {
-  albumId: string | null;
+  // albumId: string | null;
 
   @Output() reviewSave = new EventEmitter<Review>();
   rating: number = 1;
@@ -22,7 +22,7 @@ export class AddReviewFormComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {this.albumId = paramMap.get('id')})
+    // this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {this.albumId = paramMap.get('id')})
   }
 
   submit = (): void => {
@@ -37,7 +37,7 @@ export class AddReviewFormComponent implements OnInit {
     this.recommendation = false;
   };
 
-  viewReviewsRedirect(): void {
-    this.router.navigate(['review/all']);
-  }
+  // viewReviewsRedirect(): void {
+  //   this.router.navigate(['review/all']);
+  // }
 }
