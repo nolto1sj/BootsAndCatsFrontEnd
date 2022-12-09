1a0a57af3fbb4fe279a4da3f181371ab98d44853
MacobJagyar <jacobmagyar95@gmail.com>
2022-12-08 23:36:30 -0500

    change username and password functionality in settings

--- src/app/services/boots-and-cats-backend.service.ts
+++ src/app/services/boots-and-cats-backend.service.ts
@@ -1,41 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '../interfaces/user'
+import { User } from '../interfaces/user';
 import { Observable } from 'rxjs';
 import { Review } from '../interfaces/review';
-
+import { BootsAndCatsService } from './boots-and-cats.service';
 
 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BootsAndCatsBackendService {
-
   //Each developer will have to modify per-swagger
   backendURL: string = 'https://localhost:7274/api';
 
-  constructor(private httpClient: HttpClient) { }
-    //Method to get reviews
-    getReviews = (): Observable<Review[]> => {
-      return this.httpClient.get<Review[]>(this.backendURL + "/reviews")
-    }
-  
-    //Method to add a review
-    addReview = (review: Review): Observable<Review> =>{
-      return this.httpClient.post<Review>(this.backendURL + "/reviews/add", review)
-    }
-  
-    //Method to delete a review
-    deleteReview = (id: number): Observable<void> => {
-      return this.httpClient.delete<void>(this.backendURL + "/reviews/" + id);
-    }
-  
-    //get user by id
-    getUserProfile = (id: number): Observable<User> => {
-      return this.httpClient.get<User>(this.backendURL + "/Users/" + id);
-    }
+  constructor(
+    private httpClient: HttpClient,
+    private frontEndService: BootsAndCatsService
+  ) {}
+  //Method to get reviews
+  getReviews = (): Observable<Review[]> => {
+    return this.httpClient.get<Review[]>(this.backendURL + '/reviews');
+  };
 
+<<<<<<< Updated upstream
     //get all users
     getAllUser = (): Observable<User[]> => {
       return this.httpClient.get<User[]>(this.backendURL + "/Users/users");
@@ -49,3 +37,51 @@ export class BootsAndCatsBackendService {
 
   }
 
+=======
+  //Method to add a review
+  addReview = (review: Review): Observable<Review> => {
+    return this.httpClient.post<Review>(
+      this.backendURL + '/reviews/add',
+      review
+    );
+  };
+
+  //Method to delete a review
+  deleteReview = (id: number): Observable<void> => {
+    return this.httpClient.delete<void>(this.backendURL + '/reviews/' + id);
+  };
+
+  //get user by id
+  getUserProfile = (id: number): Observable<User> => {
+    return this.httpClient.get<User>(this.backendURL + '/Users/' + id);
+  };
+
+  //get all users
+  getAllUser = (): Observable<User[]> => {
+    return this.httpClient.get<User[]>(this.backendURL + '/Users/users');
+  };
+
+  //Method to signup
+  signup = (user: User): Observable<User> => {
+    return this.httpClient.post<any>(this.backendURL + '/Users', user);
+  };
+
+  UpdateByUserName = (user: User): Observable<User> => {
+    return this.httpClient.put<any>(this.backendURL + '/Users', user);
+  };
+
+  UpdateUser = (user: User): Observable<void> => {
+    return this.httpClient.put<void>(
+      this.backendURL + '/Users/changeuser',
+      user
+    );
+  };
+
+  UpdatePass = (user: User): Observable<void> => {
+    return this.httpClient.put<void>(
+      this.backendURL + '/Users/changepass',
+      user
+    );
+  };
+}
+>>>>>>> Stashed changes
