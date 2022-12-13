ccf19789d7ad21be7111e4eca57d9cefce07db27
nolto1sj <sjnolton@gmail.com>
2022-12-12 22:13:51 -0500

    tried

--- src/app/album-review/album-review.component.ts
+++ src/app/album-review/album-review.component.ts
@@ -1,9 +1,12 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Albums } from '../interfaces/album';
+
 import { Review } from '../interfaces/review';
 import { BootsAndCatsBackendService } from '../services/boots-and-cats-backend.service';
 import { BootsAndCatsService } from '../services/boots-and-cats.service';
 import { Router } from '@angular/router';
+import { User } from '../interfaces/user';
+import { SpotifyService } from '../services/spotify.service';
+import { AlbumByIdResponse } from '../interfaces/album-by-id-response';
 
 @Component({
   selector: 'app-album-review',
@@ -11,20 +14,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./album-review.component.css']
 })
 export class AlbumReviewComponent implements OnInit {
+  albumAPIResponse: AlbumByIdResponse = {} as AlbumByIdResponse;
+  albumArray: AlbumByIdResponse[] = [];
+  x: number = 0;
   reviews: Review[] = [];
+  userArray: User[] = [];
 
   constructor(private service: BootsAndCatsBackendService,
     public frontEndService: BootsAndCatsService,
-    private router: Router
+    private router: Router,
+    private spotifyservice: SpotifyService
   ) { }
 
-  ngOnInit(): void {
-    this.loadReviews();
+  async ngOnInit(): Promise<void> {
+    await this.loadReviews();
+    this.loadUsers();
+    console.log(this.reviews.length);
   }
   
+  loadUsers = (): void => {
+    this.service.getAllUser().subscribe((data: User[]) => this.userArray = data);
+  }
+
   //Method to load reviews
-  loadReviews = (): void => {
+  loadReviews = async (): Promise<void> => {
     this.service.getReviews().subscribe((data: Review[]) => this.reviews = data);
+    
+    for (let review of this.reviews){
+      (await this.getAlbumById(review.albumId));
+    }
+    console.log(this.albumAPIResponse);
+    
   }
 
   //Method to delete review and load reviews afterwards
@@ -41,6 +61,14 @@ export class AlbumReviewComponent implements OnInit {
 
   addReviewRedirect(): void {
     this.router.navigate(['/albums']);
+  }
+
+  findIndex = (review: Review): number => {
+    return this.userArray.findIndex(x => x.id == review.userId)
+  }
+
+  async getAlbumById(albumId: string) {
+   (await this.spotifyservice.getAlbumById(albumId)).subscribe((data => this.albumAPIResponse = data));
   }
 }
 
